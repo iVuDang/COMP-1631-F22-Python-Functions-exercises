b999f8d322c2c1cdce5adaf1de42b21fefa9fb66
Vu Dang <vdang159@mtroyal.ca>
2022-11-20 14:39:07 -0700

    update

--- Comp1631_ParallelLists.py
+++ Comp1631_ParallelLists.py
@@ -20,3 +20,22 @@ print ("--------------------")
 
 
 
+"""
+Parallel Lists:
+* Each list is the same length
+* Each list maintains the original order (keeps association)
+"""
+
+number = [3, 65, 77]
+street = ["Richard Road", "Richmond Road", "Radcliffe Bay"]
+quadrant = ["SW", "SW", "SE"]
+
+address = []  # empty accumulator list
+i = 0  # iterations accumulator AND to index sequence
+
+while i < len(number):
+  address.append(f'{number[i]} {street[i]} {quadrant[i]}')
+  i += 1
+
+print(address)
+# ['3 Richard Road SW', '65 Richmond Road SW', '77 Radcliffe Bay SE']
--- Comp1631_Printing_Reassign_Retrieve_Lists.py
+++ Comp1631_Printing_Reassign_Retrieve_Lists.py
@@ -5,38 +5,67 @@ this_list = [ 4, 4.0, "4", 'four', [0,1,2,3], True ]
 
 print ("1. print the contents of a list in a single line with space:")
 print (*this_list)
+# 4 4.0 4 four [0, 1, 2, 3] True
+
 
 print ("--------------------")
+#-------------------------------------------------------------------------------
 
 
 print("2. print the contents of a list in a single line with space, with commas:")
 print (*this_list, sep = ",")
+# 4,4.0,4,four,[0, 1, 2, 3],True
 
+
+#-------------------------------------------------------------------------------
 print ("--------------------")
 
 
 print("3. printing lists in new line using *:")
 print (*this_list, sep="\n")
+# 4
+# 4.0
+# 4
+# four
+# [0, 1, 2, 3]
+# True
+
 
 print ("--------------------")
+#-------------------------------------------------------------------------------
 
 
 print("4. printing lists in new line using for loop:")
 for item in this_list:
   print(item)
-  
+# 4
+# 4.0
+# 4
+# four
+# [0, 1, 2, 3]
+# True
+
+
 print ("--------------------")
+#-------------------------------------------------------------------------------
+
 
 
 print("5. printing lists in new line using for loop with range:")
 for i in range(0, len(this_list)):
   print(this_list[i])
+# 4
+# 4.0
+# 4
+# four
+# [0, 1, 2, 3]
+# True
+
+
 
 print ("--------------------")
-
-
-
 #-------------------------------------------------------------------------------
+
 
 odd_nums = [1, 3, 5, 7]
 
@@ -45,25 +74,28 @@ print ("Re-assign individual item in list")
 odd_nums[0] = 55
 
 print(odd_nums)
+# [55, 3, 5, 7]
 
 
 
 print ("--------------------")
-
+#-------------------------------------------------------------------------------
 
 print ("Retrieving individual item from list")
 value_at_zero = odd_nums[0]
 
 print(value_at_zero)
+# 55
 
 
 
 print ("--------------------")
+#-------------------------------------------------------------------------------
 
 
 print ("Calculate with items in list")
 print (2 * odd_nums [2] + value_at_zero)
-
+# 65
 
 
 print ("--------------------")
